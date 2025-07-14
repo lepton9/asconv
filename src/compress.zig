28f40f436878f3bda85be08c2a0e694a67ff6a74
lepton <tonihirvonen00@gmail.com>
2025-07-14 22:42:22 +0300

    feat: loading image

--- src/compress.zig
+++ src/compress.zig
@@ -1,26 +1,33 @@
 const std = @import("std");
+const stb = @import("stb_image");
 
-pub const image = struct {
+pub const Image = struct {
     height: u32,
     widht: u32,
     pixels: [][]u32,
+    raw_image: *stb.ImageRaw = undefined,
 
-    pub fn init(allocator: *std.mem.Allocator, height: u32, width: u32) !*image {
-        var img = try allocator.create(image);
+    pub fn init(allocator: *std.mem.Allocator, height: u32, width: u32) !*Image {
+        var img = try allocator.create(Image);
         img.height = height;
         img.widht = width;
         img.pixels = try allocator.alloc([]u32, height);
         for (img.pixels) |*row| {
             row.* = try allocator.alloc(u32, width);
+            @memset(row.*, 0);
         }
+        img.raw_image = try allocator.create(stb.ImageRaw);
+        img.raw_image.* = stb.ImageRaw{};
         return img;
     }
 
-    pub fn deinit(self: *image, allocator: *std.mem.Allocator) void {
+    pub fn deinit(self: *Image, allocator: *std.mem.Allocator) void {
         for (self.pixels) |row| {
             allocator.free(row);
         }
         allocator.free(self.pixels);
+        self.raw_image.deinit();
+        allocator.destroy(self.raw_image);
         allocator.destroy(self);
     }
 };
@@ -42,9 +49,9 @@ pub fn b(pixel: u32) u8 {
 }
 
 pub fn pixel_to_char(pixel: u32) u8 {
-    const n = 10;
+    const n: u16 = 10;
     const table = " .,:;ox%#@";
-    const index = intensity(pixel) * n / 255;
+    const index = (intensity(pixel) * n) / 255;
     return table[index];
 }
 
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const stb = @import("stb_image");
 const compress = @import("compress");
-const image = compress.image;
+const Image = compress.Image;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -8,20 +9,22 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    const img_height: u32 = 30;
-    const img_width: u32 = 30;
+    const filename = "./images/test.jpg";
+    const height: u32 = 10;
+    const width: u32 = 10;
 
     var malloc = gpa.allocator();
-    const img = try image.init(&malloc, img_height, img_width);
-    defer image.deinit(img, &malloc);
-
-    var mat: [img_height][img_width]u32 = .{.{0} ** img_width} ** img_height;
-
-    for (&mat) |row| {
+    const img = try Image.init(&malloc, height, width);
+    defer Image.deinit(img, &malloc);
+    for (img.pixels) |row| {
         for (row) |pixel| {
             try stdout.print("{c} ", .{compress.pixel_to_char(pixel)});
         }
         try stdout.print("\n", .{});
     }
     try bw.flush();
+
+    img.raw_image.* = try stb.load_image(filename, null);
+    std.debug.print("Image: {s}\n", .{filename});
+    std.debug.print("Image of size {d}x{d} with {d} channels\n", .{ img.raw_image.width, img.raw_image.height, img.raw_image.nchan });
 }
