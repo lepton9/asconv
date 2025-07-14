84b71ee50e94b0aa111763d47f2bede8d6d38d73
lepton <tonihirvonen00@gmail.com>
2025-07-15 01:11:07 +0300

    feat: pack pixels to u32

--- src/compress.zig
+++ src/compress.zig
@@ -55,6 +55,35 @@ pub fn pixel_to_char(pixel: u32) u8 {
     return table[index];
 }
 
+pub fn pack_pixel(r_v: u8, g_v: u8, b_v: u8, a_v: u8) u32 {
+    // 0xRRGGBBAA
+    return (@as(u32, r_v) << 24) |
+        (@as(u32, g_v) << 16) |
+        (@as(u32, b_v) << 8) |
+        @as(u32, a_v);
+}
+
+pub fn convert_to_pixel_matrix(allocator: *std.mem.Allocator, image: *stb.ImageRaw) ![][]u32 {
+    const w: usize = @intCast(image.width);
+    const h: usize = @intCast(image.height);
+    const channels: usize = @intCast(image.nchan);
+    const pixels = try allocator.alloc([]u32, h);
+    var r_v: u8, var g_v: u8, var b_v: u8, var a_v: u8 = .{ 0, 0, 0, 0 };
+
+    for (pixels, 0..) |*row, y| {
+        row.* = try allocator.alloc(u32, w);
+        for (row.*, 0..) |*pixel, x| {
+            const base = (y * w + x) * channels;
+            r_v = image.data.?[base];
+            g_v = if (image.nchan > 1) image.data.?[base + 1] else r_v;
+            b_v = if (image.nchan > 2) image.data.?[base + 2] else r_v;
+            a_v = if (image.nchan > 3) image.data.?[base + 3] else 0xFF;
+            pixel.* = pack_pixel(r_v, g_v, b_v, a_v);
+        }
+    }
+    return pixels;
+}
+
 // pub fn comp_chunk(mat: [][]u32, h: u32, w: u32) u32 {
 //     return 0;
 // }
