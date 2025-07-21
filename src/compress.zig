dc218c2a015c6f4b5b51b2d0c96484257708c3c0
lepton <tonihirvonen00@gmail.com>
2025-07-21 09:24:24 +0300

    feat: fit compress image to size

--- src/compress.zig
+++ src/compress.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const stb = @import("stb_image");
 
 pub const Image = struct {
+    name: []const u8,
     height: u32,
     widht: u32,
     pixels: [][]u32,
@@ -30,9 +31,27 @@ pub const Image = struct {
         allocator.destroy(self.raw_image);
         allocator.destroy(self);
     }
+
+    pub fn fit_image(self: *Image) void {
+        const h: u32, const w: u32 = calc_chunk_size(
+            @intCast(self.raw_image.height),
+            @intCast(self.raw_image.width),
+            self.height,
+            self.widht,
+        );
+        if (self.raw_image.data == null) return;
+        const pixels = convert_to_pixel_matrix(&std.heap.page_allocator, self.raw_image) catch return;
+        for (self.pixels, 0..) |row, r_i| {
+            for (row, 0..) |*pixel, c_i| {
+                pixel.* = comp_chunk(pixels, @intCast(r_i), @intCast(c_i), h, w);
+            }
+        }
+    }
 };
 
 pub fn intensity(pixel: u32) u8 {
+    return @truncate((0x000000FF & pixel));
+}
     return @truncate((0xFF000000 & pixel) >> 8 * 3);
 }
 
@@ -63,7 +82,7 @@ pub fn pack_pixel(r_v: u8, g_v: u8, b_v: u8, a_v: u8) u32 {
         @as(u32, a_v);
 }
 
-pub fn convert_to_pixel_matrix(allocator: *std.mem.Allocator, image: *stb.ImageRaw) ![][]u32 {
+pub fn convert_to_pixel_matrix(allocator: *const std.mem.Allocator, image: *stb.ImageRaw) ![][]u32 {
     const w: usize = @intCast(image.width);
     const h: usize = @intCast(image.height);
     const channels: usize = @intCast(image.nchan);
@@ -75,15 +94,26 @@ pub fn convert_to_pixel_matrix(allocator: *std.mem.Allocator, image: *stb.ImageR
         for (row.*, 0..) |*pixel, x| {
             const base = (y * w + x) * channels;
             r_v = image.data.?[base];
-            g_v = if (image.nchan > 1) image.data.?[base + 1] else r_v;
-            b_v = if (image.nchan > 2) image.data.?[base + 2] else r_v;
-            a_v = if (image.nchan > 3) image.data.?[base + 3] else 0xFF;
+            g_v = if (channels > 1) image.data.?[base + 1] else r_v;
+            b_v = if (channels > 2) image.data.?[base + 2] else r_v;
+            a_v = if (channels > 3) image.data.?[base + 3] else 0xFF;
             pixel.* = pack_pixel(r_v, g_v, b_v, a_v);
         }
     }
     return pixels;
 }
 
-// pub fn comp_chunk(mat: [][]u32, h: u32, w: u32) u32 {
-//     return 0;
-// }
+pub fn calc_chunk_size(h: u32, w: u32, h_new: u32, w_new: u32) struct { u32, u32 } {
+    return .{ h / h_new, w / w_new };
+}
+
+pub fn comp_chunk(mat: [][]u32, row: u32, col: u32, h: u32, w: u32) u32 {
+    if (h * w == 0) return 0;
+    var sum: u64 = 0;
+    for (0..h) |i| {
+        for (0..w) |j| {
+            sum += mat[row + i][col + j];
+        }
+    }
+    return @intCast(sum / (h * w));
+}
