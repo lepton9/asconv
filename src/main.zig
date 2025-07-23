d9925fa26be43e525cf1c0deafd025cff46197cb
lepton <tonihirvonen00@gmail.com>
2025-07-24 02:34:14 +0300

    feat: handle command

--- src/main.zig
+++ src/main.zig
@@ -96,28 +96,109 @@ fn handle_cli(cli_result: cli.ResultCli) ?cli.Cli {
     };
 }
 
-pub fn main() !void {
+fn size(cli_: *cli.Cli) !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var malloc = gpa.allocator();
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    const filename = "./images/test.jpg";
-    const height: u32 = 10;
-    const width: u32 = 10;
+    const filename = cli_.global_args orelse "";
+    const img = try Image.init(&malloc, 0, 0);
+    defer Image.deinit(img, &malloc);
+    img.raw_image.* = try stb.load_image(filename, null);
+    img.name = filename;
 
+    try stdout.print("Image: {s}\n", .{filename});
+    try stdout.print("Size: {d}x{d}\n", .{ img.raw_image.width, img.raw_image.height });
+    try stdout.print("Channels: {d}\n", .{img.raw_image.nchan});
+    try bw.flush();
+}
+
+fn ascii(cli_: *cli.Cli) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var malloc = gpa.allocator();
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    const filename = cli_.global_args orelse "";
+    var height: u32 = 0;
+    var width: u32 = 0;
+
     const img = try Image.init(&malloc, height, width);
     defer Image.deinit(img, &malloc);
+    img.raw_image.* = try stb.load_image(filename, null);
+    img.name = filename;
+
+    const scale_opt = cli_.find_opt("scale");
+    if (scale_opt != null) {
+        const mul = try std.fmt.parseFloat(f32, scale_opt.?.arg_value.?);
+        height = @intFromFloat(@as(f32, @floatFromInt(img.raw_image.height)) * mul);
+        width = @intFromFloat(@as(f32, @floatFromInt(img.raw_image.width)) * mul);
+    } else {
+        const opt_height = cli_.find_opt("height");
+        const opt_width = cli_.find_opt("width");
+        height = if (opt_height != null)
+            std.fmt.parseInt(u32, opt_height.?.arg_value.?, 10) catch |err| {
+                try stdout.print("Error parsing height: {}\n", .{err});
+                return;
+            }
+        else
+            @as(u32, @intCast(img.raw_image.height));
+
+        width = if (opt_width != null)
+            std.fmt.parseInt(u32, opt_width.?.arg_value.?, 10) catch |err| {
+                try stdout.print("Error parsing width: {}\n", .{err});
+                return;
+            }
+        else
+            @as(u32, @intCast(img.raw_image.width));
+    }
+    try img.resize(&malloc, height, width);
+    img.fit_image();
+
     for (img.pixels) |row| {
         for (row) |pixel| {
-            try stdout.print("{c} ", .{compress.pixel_to_char(pixel)});
+            try stdout.print("{c}", .{compress.pixel_to_char(pixel)});
         }
         try stdout.print("\n", .{});
     }
     try bw.flush();
 
-    img.raw_image.* = try stb.load_image(filename, null);
-    std.debug.print("Image: {s}\n", .{filename});
-    std.debug.print("Image of size {d}x{d} with {d} channels\n", .{ img.raw_image.width, img.raw_image.height, img.raw_image.nchan });
+    try stdout.print("Image: {s}\n", .{filename});
+    try stdout.print("Image of size {d}x{d} with {d} channels\n", .{ img.raw_image.width, img.raw_image.height, img.raw_image.nchan });
+    try bw.flush();
+}
+
+fn cmd_func(cli_: *cli.Cli, args_struct: *cmd.ArgsStructure) !void {
+    const cmd_name = cli_.cmd.?.name.?;
+    if (std.mem.eql(u8, cmd_name, "size")) {
+        try size(cli_);
+    } else if (std.mem.eql(u8, cmd_name, "ascii")) {
+        try ascii(cli_);
+    } else if (std.mem.eql(u8, cmd_name, "compress")) {
+        return;
+    } else if (std.mem.eql(u8, cmd_name, "help")) {
+        args_struct.print_commands();
+    }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var malloc = gpa.allocator();
+
+    const app = try cmd.ArgsStructure.init(&malloc);
+    defer app.deinit(&malloc);
+    app.set_commands(&commands);
+    app.set_options(&options);
+
+    var args_str = try std.process.argsAlloc(malloc);
+    defer std.process.argsFree(malloc, args_str);
+    const args = try arg.parse_args(args_str[1..]);
+
+    const cli_result = cli.validate_parsed_args(args, app);
+    var cli_ = handle_cli(cli_result) orelse return;
+
+    try cmd_func(&cli_, app);
 }
